e0e8b72fcfd4c9db1c832ea29ac0ad8213b5fba9
artifact <artifact@artifact.com>
2025-03-23 21:25:29 +0100

    Renommer application en app dans wsgi.py pour Vercel

--- backend/TME_webAPI_DAAR/mySearchEngine/mySearchEngine/wsgi.py
+++ backend/TME_webAPI_DAAR/mySearchEngine/mySearchEngine/wsgi.py
@@ -3,4 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.TME_webAPI_DAAR.mySearchEngine.mySearchEngine.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
