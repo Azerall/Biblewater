7254d81dad0a374a91c90031fd2a7e01b28a1c2f
artifact <artifact@artifact.com>
2025-03-23 20:57:30 +0100

    Corriger build.sh avec chemin absolu et logs

--- backend/TME_webAPI_DAAR/mySearchEngine/build.sh
+++ backend/TME_webAPI_DAAR/mySearchEngine/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-echo "Installing dependencies..."
-pip install -r requirements.txt --verbose
-echo "Dependencies installed."
+echo "Starting build process..."
+echo "Current directory: $(/bin/pwd)"
+echo "Listing files: $(/bin/ls -la)"
+pip install -r /vercel/path0/backend/TME_webAPI_DAAR/mySearchEngine/requirements.txt --verbose
+echo "Installation completed."
