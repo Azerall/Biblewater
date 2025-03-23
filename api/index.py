416dfd2832b943e17695ff13198d873e9d4a9834
artifact <artifact@artifact.com>
2025-03-23 20:48:38 +0100

    Utiliser un handler WSGI minimal pour Vercel

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
-def handler(event, context):
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": "{\"status\": \"OK\"}"
-    }
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'application/json')]
+    start_response(status, headers)
+    return [b'{"status": "OK"}']
