a14f060126b53d0140c8f9443eccf54693c14b87
artifact <artifact@artifact.com>
2025-03-23 21:06:17 +0100

    adjust requirements.txt

--- backend/TME_webAPI_DAAR/mySearchEngine/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting build process..."
-echo "Current directory: $(/bin/pwd)"
-echo "Listing files: $(/bin/ls -la)"
-pip install -r /vercel/path0/backend/TME_webAPI_DAAR/mySearchEngine/requirements.txt --verbose
-echo "Installation completed."
