7cd3b05d7ed20d18a192f212c6cfdd1308c598a0
artifact <artifact@artifact.com>
2025-03-23 20:45:41 +0100

    Corriger le handler serverless pour Vercel

--- api/index.py
+++ api/index.py
@@ -1,8 +1,6 @@
 def handler(event, context):
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": '{"status": "OK"}'
+        "headers": {"Content-Type": "application/json"},
+        "body": "{\"status\": \"OK\"}"
     }
