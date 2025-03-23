fef2a80fb4720bffc77a7bdd52de088a38f143d8
artifact <artifact@artifact.com>
2025-03-23 21:22:20 +0100

    Corriger le champ name dans MygutenbergConfig pour Vercel

--- backend/TME_webAPI_DAAR/mySearchEngine/mygutenberg/apps.py
+++ backend/TME_webAPI_DAAR/mySearchEngine/mygutenberg/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class MygutenbergConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mygutenberg'
+    name = 'backend.TME_webAPI_DAAR.mySearchEngine.mygutenberg'
