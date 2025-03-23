3c501d725112028594b6b65244174980e9831430
artifact <artifact@artifact.com>
2025-03-23 21:28:37 +0100

    Ajuster sys.path dans wsgi.py pour résoudre ModuleNotFoundError

--- backend/TME_webAPI_DAAR/mySearchEngine/mySearchEngine/wsgi.py
+++ backend/TME_webAPI_DAAR/mySearchEngine/mySearchEngine/wsgi.py
@@ -1,6 +1,9 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.TME_webAPI_DAAR.mySearchEngine.mySearchEngine.settings')
+# Ajouter le chemin parent au sys.path pour que 'mySearchEngine' soit trouvé
+sys.path.append('/var/task/backend/TME_webAPI_DAAR/mySearchEngine')
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.TME_webAPI_DAAR.mySearchEngine.mySearchEngine.settings')
 app = get_wsgi_application()
